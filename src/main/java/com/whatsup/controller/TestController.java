1b26d170d0c5131e999f825f30735c9d879d999a
DelMonroe <delenor@gmail.com>
2017-07-05 14:25:38 -0500

    added test controller to push to git

---
+++ src/main/java/com/whatsup/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.whatsup.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by DelMonroe on 7/2/17.
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello! ... I am working!!!!";
+    }
+
+}
