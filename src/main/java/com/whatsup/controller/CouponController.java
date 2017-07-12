091c020be39dcd93f32f79d36bcae0baffd8a49f
Moses Franco <mosesf@outlook.com>
2017-07-12 15:11:45 -0500

    Forgot to add model, now front end view actually works

--- src/main/java/com/whatsup/controller/CouponController.java
+++ src/main/java/com/whatsup/controller/CouponController.java
@@ -4,6 +4,7 @@ import com.whatsup.models.Coupon;
 import com.whatsup.repositories.CouponsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,8 @@ public class CouponController {
 	CouponsRepository couponDao;
 
 	@GetMapping("/coupon/create")
-	public String formsCoupon(){
+	public String formsCoupon(Model model){
+		model.addAttribute("coupon", new Coupon());
 		return "coupon/create";
 	}
 
