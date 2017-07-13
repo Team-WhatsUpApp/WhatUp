1f2c3f7253515ab64cc8ac2b5e788ca83b506338
Moses Franco <mosesf@outlook.com>
2017-07-13 15:45:25 -0500

    New controller

---
+++ src/main/java/com/whatsup/controller/MapController.java
@@ -0,0 +1,40 @@
+package com.whatsup.controller;
+
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonArrayFormatVisitor;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.oracle.javafx.jmx.json.JSONException;
+import com.whatsup.models.Location;
+import com.whatsup.repositories.LocationsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.JsonObjectSerializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+/**
+ * Created by mosesfranco on 7/13/17
+ * Codeup
+ * Pinnacles
+ */
+@Controller
+public class MapController {
+
+	@Autowired
+	private LocationsRepository locationsDao;
+
+	@GetMapping("/maps/{lon}/{lat}/{dist}")
+	public String loadMap(@PathVariable double lon, @PathVariable double lat, @PathVariable double dist, Model model) throws JSONException{
+		Iterable<Location> results = locationsDao.getLocationsMap(lon, lat, dist/2);
+		model.addAttribute("results", results);
+		return "map/map";
+	}
+
+
+}
