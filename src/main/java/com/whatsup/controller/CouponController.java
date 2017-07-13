1752e4f0fcacd2789d6dd652d6f919ce5e8d24fa
Moses Franco <mosesf@outlook.com>
2017-07-13 08:07:58 -0500

    Untested version of the controller, but should work in theory

--- src/main/java/com/whatsup/controller/CouponController.java
+++ src/main/java/com/whatsup/controller/CouponController.java
@@ -1,8 +1,14 @@
 package com.whatsup.controller;
 
+import com.sun.tools.corba.se.idl.ValueEntry;
 import com.whatsup.models.Coupon;
+import com.whatsup.models.User;
+import com.whatsup.models.Vendor;
 import com.whatsup.repositories.CouponsRepository;
+import com.whatsup.repositories.UsersRepository;
+import com.whatsup.repositories.VendorsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,15 +24,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class CouponController {
 	@Autowired
 	CouponsRepository couponDao;
+	@Autowired
+	UsersRepository userDao;
+	@Autowired
+	VendorsRepository vendorDao;
 
 	@GetMapping("/coupon/create")
-	public String formsCoupon(Model model){
+	public String formsCoupon(Model model) {
 		model.addAttribute("coupon", new Coupon());
 		return "coupon/create";
 	}
 
 	@PostMapping("/coupon/create")
-	public String createCoupon(@ModelAttribute Coupon coupon){
+	public String createCoupon(@ModelAttribute Coupon coupon) {
+		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Vendor vendor = vendorDao.findByOwner(user);
+		coupon.setOwner(vendor);
 		couponDao.save(coupon);
 		return "coupon";
 	}
