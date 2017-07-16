881469d3d936ca662ccd4807fe994c1db69888d3
Moses Franco <mosesf@outlook.com>
2017-07-16 00:05:01 -0500

    Reformatting, I suspect, but I'm not too sure, the difference markers is unclear

--- src/main/java/com/whatsup/controller/MapController.java
+++ src/main/java/com/whatsup/controller/MapController.java
@@ -1,18 +1,13 @@
 package com.whatsup.controller;
 
-
-
 import com.whatsup.models.Location;
 import com.whatsup.repositories.LocationsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 /**
  * Created by mosesfranco on 7/13/17
@@ -26,7 +21,7 @@ public class MapController {
 	private LocationsRepository locationsDao;
 
 	@GetMapping("/maps/geo")
-	public String loadMap(/*@PathVariable double lon, @PathVariable double lat, @PathVariable double dist, Model model*/){
+	public String loadMap(/*@PathVariable double lon, @PathVariable double lat, @PathVariable double dist, Model model*/) {
 //		Iterable<Location> results = locationsDao.getLocationsMap(lon, lat, dist/2);
 //		model.addAttribute("results", results);
 		return "map/map";
@@ -34,8 +29,9 @@ public class MapController {
 
 
 	@GetMapping("/locations.json/{lon}/{lat}/{dist}")
-	public @ResponseBody Iterable<Location>  locations(@PathVariable double lon, @PathVariable double lat, @PathVariable double dist, Model model) {
-		return locationsDao.getLocationsMap(lon, lat, dist/2);
+	public @ResponseBody
+	Iterable<Location> locations(@PathVariable double lon, @PathVariable double lat, @PathVariable double dist, Model model) {
+		return locationsDao.getLocationsMap(lon, lat, dist / 2);
 		//model.addAttribute("results", results);
 		//return "map/map";
 	}
