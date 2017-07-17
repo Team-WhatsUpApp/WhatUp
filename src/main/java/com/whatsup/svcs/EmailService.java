da2c124a37c5ba1116b4d5ef12f9f157450e0531
JSR7 <jsran01@yahoo.com>
2017-07-17 14:38:30 -0500

    deleted unnessasary code   verified with del

--- src/main/java/com/whatsup/svcs/EmailService.java
+++ src/main/java/com/whatsup/svcs/EmailService.java
@@ -1,13 +1,13 @@
-package com.whatsup.svcs;
-
-import org.springframework.mail.SimpleMailMessage;
-
-/**
- * Created by DelMonroe on 7/15/17.
- */
-
-
-
-public interface EmailService {
-    public void sendEmail(SimpleMailMessage email);
-}
+//package com.whatsup.svcs;
+//
+//import org.springframework.mail.SimpleMailMessage;
+//
+///**
+// * Created by DelMonroe on 7/15/17.
+// */
+//
+//
+//
+//public interface EmailService {
+//    public void sendEmail(SimpleMailMessage email);
+//}
