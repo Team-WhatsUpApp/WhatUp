038d55facc97f251168460c0a0fcb5bc1b786990
Moses Franco <mosesf@outlook.com>
2017-07-17 17:18:21 -0500

    Fixed href to single location page

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -2,7 +2,7 @@
  * Created by mosesfranco on 7/14/17.
  */
 
-(function(){
+(function () {
     "use strict";
 
     var map;
@@ -55,7 +55,8 @@
                         '<b class="name">' + location.name + '</b>' +
                         '<p><span>' + location.address + '</span><br/>' +
                         '<a href="tel:' + location.phone + '"><span>Call</span></a><br/>' +
-                        '<a href="' + location.url + '"><span>More</span></a><br/>' +
+                        '<a href="' + location.url + '"><span>Yelp</span></a><br/>' +
+                        '<a href="' + '/places/' + location.id + '"><span>More</span></a><br/>' +
 //                        '<a target="_blank" href="https://maps.google.com/maps/dir/api=1?&amp;query=' + location.x + ',' + location.y + '&amp;z=16&amp;t=m&amp;hl=en-US&amp;gl=US&amp;mapclient=apiv3"><span>View on Google Maps</span></a>' +
                         '</p>' +
                         '</div>' +
